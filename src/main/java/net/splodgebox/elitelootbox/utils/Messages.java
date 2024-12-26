a851a5e70c5084ac915ca41b0053ab2a8366697e
Splodgebox <splodegbox786@gmail.com>
2024-12-26 01:15:15 +0000

    updated license

--- src/main/java/net/splodgebox/elitelootbox/utils/Messages.java
+++ src/main/java/net/splodgebox/elitelootbox/utils/Messages.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2024. ElitePlugins License.
+ * Permission is hereby granted to use this software (the "Software") for personal purposes or on private servers. Redistribution,
+ * modification for distribution, or sale of this software is strictly prohibited.
+ *
+ * This software is provided "as is," without warranty of any kind, express or implied, including but not limited to the warranties of
+ * merchantability, fitness for a particular purpose, or non-infringement. In no event shall the author or copyright holder be liable for
+ * any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection
+ * with the Software or its use.
+ *
+ * See LICENSE.txt for details.
+ */
+
 package net.splodgebox.elitelootbox.utils;
 
 import net.splodgebox.eliteapi.message.Message;
